3b104ba2363aff60a54319829af4a7e17c157258
alita-moore <alita.moore805@gmail.com>
2020-09-13 18:40:58 -0500

    initial setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+import {test, expect, it} from "enzyme";
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+})
+
+it("runs test", () => {
+  expect(1).toBe(1)
 });
