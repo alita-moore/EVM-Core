453127c0f8c6f80c750df35c3ac943a721fc1d36
alita-moore <alita.moore805@gmail.com>
2020-09-13 23:19:44 -0500

    minor xterm logic changes

--- src/components/Terminal/index.tsx
+++ src/components/Terminal/index.tsx
@@ -1,32 +1,62 @@
-import React, { CSSProperties, useState } from "react";
+import React, { CSSProperties, useState, useRef, useEffect } from "react";
 import { ScriptProps } from "../../types";
 import { XTerm } from "xterm-for-react";
+import { ITerminalOptions } from "xterm";
 
 const TerminalComponent = (scriptProps: ScriptProps) => {
 	const { script } = scriptProps;
 	const [editing, setEditing] = useState(false);
 
 	const TerminalText = editing ? script : "";
-	// const terminal = new Terminal();
-	// const fitAddon = new FitAddon();
-	// terminal.loadAddon(fitAddon);
-	// terminal.open(document.getElementById("terminal"));
+
+	const RunButton = (
+		<button style={styles.buttonStyle} onClick={() => setEditing(!editing)}>
+			run code
+		</button>
+	);
 
 	return (
 		<div id="terminal" style={styles.container}>
-			<button
-				style={styles.buttonStyle}
-				onClick={() => setEditing(!editing)}
-			>
-				run code
-			</button>
-			<text>{TerminalText}</text>
-			<XTerm />
+			{RunButton}
+			<XTermInteractive />
 		</div>
 	);
 };
 
 export default TerminalComponent;
+
+const XTermInteractive = () => {
+	const xtermRef = useRef(null);
+	const [input, setInput] = useState("");
+
+	const options: ITerminalOptions = {
+		cursorBlink: true,
+	};
+
+	useEffect(() => {
+		xtermRef.current.terminal.writeln(
+			"Hello, and welcome to Huff Playground"
+		);
+	}, []);
+
+	const handleKey = (e) => {
+		if (e.domEvent.key === "Backspace") {
+			setInput(input.slice(0, input.length - 1));
+		} else {
+			setInput(input + e.key);
+		}
+		console.log(input);
+	};
+
+	return (
+		<XTerm
+			ref={xtermRef}
+			options={options}
+			className={"xterm xterm-message"}
+			onKey={handleKey}
+		/>
+	);
+};
 
 const styles = {
 	container: {
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './utils/serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./utils/serviceWorker";
+
+import "./css/xterm.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
